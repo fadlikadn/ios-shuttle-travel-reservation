ff6bf21c617a5af89a69e15b10a3d83b03b553ff
Fadlika Dita Nurjanto <fadlikadn@gmail.com>
2017-08-01 09:48:21 +0700

    Feature - Add login page and home page

---
+++ IOSShuttle/LoginViewController.swift
@@ -0,0 +1,39 @@
+//
+//  LoginViewController.swift
+//  IOSShuttle
+//
+//  Created by MTMAC23 on 8/1/17.
+//  Copyright © 2017 MTMAC23. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet var usernameTextView: UITextField!
+    @IBOutlet var passwordTextView: UITextField!
+    @IBOutlet var loginButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
